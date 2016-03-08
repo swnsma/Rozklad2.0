1fa9e486d5fefe5f0e4fb142d1d6eec3584ac62b
Tania Blindaruk <t.blindaruk@gmail.com>
2016-03-08 14:40:40 +0200

    Admin. Add calendar page.

---
+++ source/src/Rozklad/CalendarBundle/Controller/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by tania.
+ * Date: 8.03.16
+ * Time: 14:12
+ * @corporation Maksi
+ */
+
+namespace Rozklad\CalendarBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sonata\AdminBundle\Controller\CoreController;
+
+class AdminController extends CoreController
+{
+    public function calendarAction(Request $request)
+    {
+        return $this->render('RozkladCalendarBundle:Admin:calendar.html.twig', array(
+            'base_template'   => $this->getBaseTemplate(),
+            'admin_pool'      => $this->container->get('sonata.admin.pool'),
+            'blocks'          => $this->container->getParameter('sonata.admin.configuration.dashboard_blocks')
+        ));
+    }
+
+}
