a609b15b056230f0ba07bc87cfe75f3693097c31
Tania Blindaruk <t.blindaruk@gmail.com>
2016-04-10 00:09:34 +0300

    Code reformate

--- mvp/public/js/Calendar.js
+++ mvp/public/js/Calendar.js
@@ -5,7 +5,6 @@ function Calendar() {
     this._option = {
         schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
         weekends: false, // will hide Saturdays and Sundays
-        now: '2016-04-04 10:30:00',
         minTime: '08:00:00',
         maxTime: '14:10:00',
         monthNames: ['Cічень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Bересень', 'Жовтень', 'Листопад', 'Грудень'],
@@ -50,13 +49,13 @@ Calendar.prototype.renderCalendar = function (group, element) {
         },
         eventAfterAllRender: function (view) {
             var $element = $('#calendar');
-            if(view.name === "agendaDay"){
+            if (view.name === "agendaDay") {
                 var minute = $element.fullCalendar('getDate').format('mm');
                 minute = (minute < 30) ? '00' : '30';
-                var hour = $element.fullCalendar('getDate').format('HH')+ ':' + minute + ':00';
+                var hour = $element.fullCalendar('getDate').format('HH') + ':' + minute + ':00';
                 $('.fc-slats tr[data-time="' + hour + '"]').addClass('now')
             }
-            if(view.name === "agendaWeek") {
+            if (view.name === "agendaWeek") {
                 var today = $('#calendar').fullCalendar('getDate').format('YYYY-MM-DD');
                 $('th.fc-day-header[data-date="' + today + '"]').addClass('now');
             }
--- mvp/public/js/main.js
+++ mvp/public/js/main.js
@@ -1,42 +1,42 @@
 /**
  * Created by tania on 30.03.16.
  */
-var timetableApp = angular.module('timetableApp',[
+var timetableApp = angular.module('timetableApp', [
     'ngRoute',
     'timetableControllers'
 ]);
 
 timetableApp.config(['$routeProvider',
-    function($routeProvider) {
+    function ($routeProvider) {
         $routeProvider.
-        when('/', {
-            templateUrl: 'public/partial/main.html',
-            controller: 'MainCtrl'
-        }).
-        when('', {
-            templateUrl: 'public/partial/main.html',
-            controller: 'MainCtrl'
-        }).
-        when('/group/:group', {
-            templateUrl: 'public/partial/group.html',
-            controller: 'GroupCtrl'
-        }).
-        when('/auditory', {
-            templateUrl: 'public/partial/auditory.html',
-            controller: 'AuditoryCtrl'
-        }). 
-        otherwise({
-            redirectTo: '/'
-        });
+            when('/', {
+                templateUrl: 'public/partial/main.html',
+                controller: 'MainCtrl'
+            }).
+            when('', {
+                templateUrl: 'public/partial/main.html',
+                controller: 'MainCtrl'
+            }).
+            when('/group/:group', {
+                templateUrl: 'public/partial/group.html',
+                controller: 'GroupCtrl'
+            }).
+            when('/auditory', {
+                templateUrl: 'public/partial/auditory.html',
+                controller: 'AuditoryCtrl'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
     }]);
 
 timetableApp.directive('groupselect', function () {
     return {
         restrict: "E",
         templateUrl: "public/partial/directive/groupSelect.html",
-        controller: ['$scope','$http', function($scope,$http) {
+        controller: ['$scope', '$http', function ($scope, $http) {
             $http.get('ajaxGroup.php').success(function (data, status, headers, config) {
-                data =  data.constructor === Array ? data : [];
+                data = data.constructor === Array ? data : [];
                 $scope.groups = data ? data : [];
             }).error(function (data, status, headers, config) {
 
