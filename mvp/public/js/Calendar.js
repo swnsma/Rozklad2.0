40f67935d7b885ddaa6c37823502876d9acea912
Tania Blindaruk <t.blindaruk@gmail.com>
2016-04-11 22:19:07 +0300

    Change pair event

--- mvp/public/js/Calendar.js
+++ mvp/public/js/Calendar.js
@@ -39,29 +39,30 @@ Calendar.prototype.renderCalendar = function (group, element) {
         timeFormat: 'H:mm',// uppercase H for 24-hour clock
         axisFormat: 'H:mm',
         eventAfterRender: function (event, element, view) {
-            if(view.name==='agendaWeek') {
+            if (view.name === 'agendaWeek') {
                 function addClass($element) {
                     $element.addClass('pair');
-                    $element.addClass('date-' +event.start._i.replace(' ','-'));
+                    $element.addClass('date-' + event.start._i.replace(' ', '-'));
                 }
+
                 var $element = $(element);
                 if ($element.css('marginRight') === '20px') {
                     addClass($element);
                     $element.css({
                         'z-index': '3',
-                        'margin-bottom': '10px'
+                        'marginRight': '45px'
                     });
                 }
                 if ($element.css('left') !== '0px') {
                     addClass($element);
                     $element.css({
-                        'left': '20%'
+                        'left': '45px'
                     });
                 }
             }
         },
-        eventClick: function(calEvent, jsEvent, view) {
-            if(view.name==='agendaWeek') {
+        eventClick: function (calEvent, jsEvent, view) {
+            if (view.name === 'agendaWeek') {
                 var $self = $(this);
                 var classList = $self.attr('class').split(/\s+/);
                 var $prev = $self.prev();
@@ -70,17 +71,41 @@ Calendar.prototype.renderCalendar = function (group, element) {
                 $.each(classList, function (index, item) {
                     if (item === 'pair') {
                         $self.css({
-                            'z-index': zIndex + 1,
-                            'margin-bottom': '10px'
+                            'z-index': zIndex + 1
                         });
-                        if($prev.attr('class') && classList[index+1]===$prev.attr('class').split(/\s+/)[index+1]) {
-                            $prev.css({
-                                'margin-bottom': '0px'
+                        if ($prev.attr('class') && classList[index + 1] === $prev.attr('class').split(/\s+/)[index + 1]) {
+                            $prev.find('.fc-content').css({
+                                'display': 'none'
+                            });
+                            $($prev.append($prev.find('.fc-content .auditory').css({
+                                'position': 'absolute',
+                                'left': 2,
+                                'top': 2
+                            })));
+                            $self.find('.fc-content').css({
+                                'display': 'block'
+                            });
+                            $self.find('.fc-content .fc-time').append($self.find('.auditory'));
+                            $self.find('.auditory').css({
+                                'position': 'static'
                             });
                         }
-                        if($next.attr('class') && classList[index+1]===$next.attr('class').split(/\s+/)[index+1]) {
-                            $next.css({
-                                'margin-bottom': '0px'
+                        if ($next.attr('class') && classList[index + 1] === $next.attr('class').split(/\s+/)[index + 1]) {
+                            $next.find('.fc-content').css({
+                                'display': 'none'
+                            });
+                            $($next.append($next.find('.fc-content .auditory').css({
+                                'position': 'absolute',
+                                'right': 2,
+                                'top': 2
+                            })))
+
+                            $self.find('.fc-content').css({
+                                'display': 'block'
+                            });
+                            $self.find('.fc-content .fc-time').append($self.find('.auditory'));
+                            $self.find('.auditory').css({
+                                'position': 'static'
                             });
                         }
                     }
@@ -98,6 +123,7 @@ Calendar.prototype.renderCalendar = function (group, element) {
                 var today = moment().format('YYYY-MM-DD');
                 $('th.fc-day-header[data-date="' + today + '"]').addClass('now');
             }
+            $('.pair').click();
         },
         eventSources: [
             '/mvp/ajaxCall.php?group=' + group
